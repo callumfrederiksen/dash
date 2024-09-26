60ec837b905041043e8bcf0b9b25199d43210f66
callumfrederiksen <callumfrederiksen97@gmail.com>
2024-09-26 21:05:10 +0100

    Added reading the dash file and declared basic *.dash syntax and grouped into words

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use std::fs;
+
+fn open_file(filepath: &str) -> String {
+    let contents = fs::read_to_string(filepath).expect("sigma");
+    return contents;
+}
 fn main() {
-    println!("Hello, world!");
+    let mut words: Vec<String> = vec![]; // Initializes an empty vector of words
+    let mut word: Vec<char> = vec![]; // Initialises the first word
+    let contents: String = open_file("./dash/return.dash");
+
+    for c in contents.chars() {
+        if c != '\n' && c != ' ' && c != '\t' {
+            word.push(c);
+        } else if word != vec![] {
+            words.push(word.iter().collect());
+            word = vec![];
+        }
+    }
+
+    for x in words {
+        print!("{x}\n");
+    }
 }
